e84331fdb24a8718c14899ebfc85b5ab59982cc7
Olivier Maréchal <o.marechal@icloud.com>
2025-05-06 14:52:34 +0200

    remove themeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Providers } from "./providers";
 
 const geistSans = Geist({
@@ -40,9 +39,7 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>
-          <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light">
-            {children}
-          </ThemeProvider>
+          {children}
         </Providers>
       </body>
     </html>
